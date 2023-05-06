728a52c9bf1e63e9a86d926eb4429b48ea66b0ef
mahaubin <mahaubin@gmail.com>
2023-05-06 18:08:59 +0300

    Teste de l'axios

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,31 +1,42 @@
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
-import $ from "jquery";
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import $ from 'jquery';
+import API from '../api';
 
-import "../Assets/css/navbar.scss";
+import '../Assets/css/navbar.scss';
 
 const Navbar = () => {
+  const connexion = async () => {
+    await API.get(`webcup/test`)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   useEffect(() => {
-    $("#menu").css({
-      background: "transparent",
-      transition: "background .5s",
+    connexion();
+    $('#menu').css({
+      background: 'transparent',
+      transition: 'background .5s',
     });
-    $("#menu a").css("color", "#fff");
+    $('#menu a').css('color', '#fff');
 
-    window.addEventListener("scroll", () => {
+    window.addEventListener('scroll', () => {
       if (window.scrollY < 120) {
-        $("#menu").css({
-          background: "transparent",
-          transition: "background .5s",
+        $('#menu').css({
+          background: 'transparent',
+          transition: 'background .5s',
         });
-        $("#menu a").css("color", "#fff");
+        $('#menu a').css('color', '#fff');
       } else {
-        $("#menu").css({
-          opacity: "0.8",
-          background: "black",
-          transition: "background .5s",
+        $('#menu').css({
+          opacity: '0.8',
+          background: 'black',
+          transition: 'background .5s',
         });
-        $("#menu a").css("color", "#fff");
+        $('#menu a').css('color', '#fff');
       }
     });
   }, []);
@@ -36,13 +47,13 @@ const Navbar = () => {
         <div className="logo">
           <span
             style={{
-              color: "#ed156d",
-              paddingLeft: "5px",
-              fontWeight: "bolder",
+              color: '#ed156d',
+              paddingLeft: '5px',
+              fontWeight: 'bolder',
             }}
           >
             N
-          </span>{" "}
+          </span>{' '}
           OFY
         </div>
         <div id="navigation">
@@ -67,7 +78,9 @@ const Navbar = () => {
           <span class="line l3"></span>
         </button>
         <Link to="/login">
-          <button className="se_connecter">Se connecter</button>
+          <button className="se_connecter" s>
+            Se connecter
+          </button>
         </Link>
       </nav>
     </>
