6e30e54e59279f2deac2a345923d4acb5febc234
AntsaTsaroana <antsaroana@gmail.com>
2023-05-06 15:12:18 +0300

    origin main

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,73 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import $ from "jquery";
 
-import "../Assets/css/navbar.scss"
+import "../Assets/css/navbar.scss";
 
 const Navbar = () => {
+  useEffect(() => {
+    $("#menu").css({
+      background: "transparent",
+      transition: "background .5s",
+    });
+    $("#menu a").css("color", "#fff");
+
+    window.addEventListener("scroll", () => {
+      if (window.scrollY < 120) {
+        $("#menu").css({
+          background: "transparent",
+          transition: "background .5s",
+        });
+        $("#menu a").css("color", "#fff");
+      } else {
+        $("#menu").css({
+          opacity: "0.8",
+          background: "black",
+          transition: "background .5s",
+        });
+        $("#menu a").css("color", "#fff");
+      }
+    });
+  }, []);
+
   return (
     <>
-      <nav></nav>
+      <nav id="menu">
+        <div className="logo">
+          Dear{" "}
+          <span
+            style={{
+              color: "#ed156d",
+              paddingLeft: "5px",
+              fontWeight: "bolder",
+            }}
+          >
+            ME.
+          </span>{" "}
+        </div>
+        <div id="navigation">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/service">Services</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contactUs">Contact Us</Link>
+            </li>
+          </ul>
+        </div>
+        {/* <button type="button" aria-label="burger Menu" class="toggle-nav">
+          <span class="line l1"></span>
+          <span class="line l2"></span>
+          <span class="line l3"></span>
+        </button> */}
+        <Link to="/login" className="btn">I am a button</Link>
+      </nav>
     </>
   );
 };
--- src/Pages/Accueil.jsx
+++ src/Pages/Accueil.jsx
@@ -6,46 +6,47 @@ import "../Assets/css/pageLoadReveal.scss";
 import "../Assets/css/accueil.scss";
 
 const Accueil = () => {
-  gsap.registerPlugin(ScrollTrigger);
+  // gsap.registerPlugin(ScrollTrigger);
 
-  // ALL REFERENCES :
+  // // ALL REFERENCES :
   const container = useRef();
 
-  useLayoutEffect(() => {
-    // ALL ANIMATION :
-    const ctx = gsap.context(() => {
-      const TLLOAD = gsap.timeline({
-        default: {
-          ease: "power2",
-        },
-      });
+  // useLayoutEffect(() => {
+  //   // ALL ANIMATION :
+  //   const ctx = gsap.context(() => {
+  //     const TLLOAD = gsap.timeline({
+  //       default: {
+  //         ease: "power2",
+  //       },
+  //     });
 
-      TLLOAD.to(".images-container", {
-        height: 400,
-        duration: 1.3,
-        delay: 0.4,
-      })
-        .to(".bloc-txt", { height: "auto", duration: 0.6 }, "-=0.8")
-        .to(".bloc-txt h2", { y: 0 }, "-=0.6")
-        .to(".f2", { y: 0, duration: 0.6 })
-        .to(".flip-img1", { display: "none", duration: 0 })
-        .to(".f2", { y: "-100%" })
-        .to(".load-container", { y: "-100%", duration: 0.5, delay: 0.7 })
-        .add(() => {
-          document.querySelector(".load-container").style.display = "none";
-        })
-        .to(`h1`, {
-          top: 0,
-          duration: 0.5,
-        });
-    }, container);
+  //     TLLOAD.to(".images-container", {
+  //       height: 400,
+  //       duration: 1.3,
+  //       delay: 0.4,
+  //     })
+  //       .to(".bloc-txt", { height: "auto", duration: 0.6 }, "-=0.8")
+  //       .to(".bloc-txt h2", { y: 0 }, "-=0.6")
+  //       .to(".f2", { y: 0, duration: 0.6 })
+  //       .to(".flip-img1", { display: "none", duration: 0 })
+  //       .to(".f2", { y: "-100%" })
+  //       .to(".load-container", { y: "-100%", duration: 0.5, delay: 0.7 })
+  //       .add(() => {
+  //         document.querySelector(".load-container").style.display = "none";
+  //       })
+  //       .to(`h1`, {
+  //         top: 0,
+  //         duration: 0.5,
+  //       });
+  //   }, container);
 
-    return () => ctx.revert();
-  }, []);
+  //   return () => ctx.revert();
+  // }, []);
 
   return (
     <div ref={container}>
-      <div className="load-container">
+      {/* LOADING OVERLAY */}
+      {/* <div className="load-container">
         <div className="anim-bloc">
           <div className="images-container">
             <div className="img-flip flip-img2"></div>
@@ -57,7 +58,9 @@ const Accueil = () => {
             <h2>Adamo.</h2>
           </div>
         </div>
-      </div>
+      </div> */}
+
+      {/* ACCUEIL */}
       <div className="container">
         <h1>ACCUEIL PAPI</h1>
       </div>
